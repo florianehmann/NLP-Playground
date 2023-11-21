2c65f104d779f16dce832e8471c41ce143ef8c20
Florian Ehmann <kontakt@florianehmann.de>
2023-11-21 17:55:16 +0100

    Add Basic Query Form to Webapp

--- webapp/app/base/routes.py
+++ webapp/app/base/routes.py
@@ -1,8 +1,15 @@
-from flask import render_template
+from flask import flash, render_template
 from app.base import bp
+from app.base.forms import QueryForm
 
 
-@bp.route('/')
-@bp.route('/index')
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('base/index.html')
+    form = QueryForm()
+    result = None
+
+    if form.validate_on_submit():
+        result = form.tweet_text.data
+
+    return render_template('base/index.html', form=form, result=result)
