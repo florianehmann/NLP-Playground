445b20baafaff964a19ab40b629379105503885e
Florian Ehmann <kontakt@florianehmann.de>
2023-11-26 22:41:08 +0100

    Add Inferece Using Huggingface API

--- webapp/app/base/routes.py
+++ webapp/app/base/routes.py
@@ -1,15 +1,28 @@
-from flask import flash, render_template
 from app.base import bp
 from app.base.forms import QueryForm
+from app.inference import query_model, ModelQueryException
+from flask import flash, render_template
 
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
     form = QueryForm()
-    result = None
+
+    render = None
 
     if form.validate_on_submit():
-        result = form.tweet_text.data
+        try:
+            tweet = form.tweet_text.data
+            classifications = query_model(tweet)
+            emotion = classifications[0]['label']
+            confidence = f"{classifications[0]['score'] * 100:.1f}"
 
-    return render_template('base/index.html', form=form, result=result)
+            render = render_template('base/index.html', form=form, tweet=tweet, emotion=emotion, confidence=confidence)
+        except ModelQueryException as e:
+            flash(f"Model Query Exception: {e}")
+
+    if render is None:
+        render = render_template('base/index.html', form=form)
+
+    return render
